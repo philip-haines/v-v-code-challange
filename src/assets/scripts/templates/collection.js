fc357c29c5698c6f3439effdcb96f9487091d3da
Phil Haines <phil.haines92@gmail.com>
2021-06-13 11:13:49 -0600

    linked collection scss page to liquid file through collection js page

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -1,6 +1,6 @@
 import "../theme";
 import getAllProducts from "../graphql/collection-starter-code";
-
+import "../../styles/templates/collection.scss";
 // getAllProducts('test-collection') => returns a Promise, which resolves to an Array of Product Objects
 
 // Your Code Here
