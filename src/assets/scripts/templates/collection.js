de07c1c8c81f7c7d756239923209f26eaa0610ca
Phil Haines <phil.haines92@gmail.com>
2021-06-13 13:44:59 -0600

    added functionality for sort drop down. Deleted old drop down, dynamically render options with liquid. Use JS to find string of option then build new URI to display products in desired order

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -5,3 +5,27 @@ import "../../styles/templates/collection.scss";
 
 // Your Code Here
 getAllProducts("test-collection").then((products) => console.log(products));
+
+Shopify.queryParams = {};
+if (location.search.length) {
+	for (
+		let sortByValue,
+			i = 0,
+			sortByOptionRaw = location.search.substr(1).split("&");
+		i < sortByOptionRaw.length;
+		i++
+	) {
+		console.log(location.search.substr(1).split("&"));
+		sortByValue = sortByOptionRaw[i].split("=");
+		if (sortByValue.length > 1) {
+			Shopify.queryParams[decodeURIComponent(sortByValue[0])] =
+				decodeURIComponent(sortByValue[1]);
+		}
+	}
+}
+
+document.querySelector(".sort-by").addEventListener("change", function (e) {
+	const value = e.currentTarget.value;
+	Shopify.queryParams.sort_by = value;
+	location.search = new URLSearchParams(Shopify.queryParams).toString();
+});
