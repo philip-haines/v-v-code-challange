30e47d12ba89512f413607a1c856d7b2d4c57726
Phil Haines <phil.haines92@gmail.com>
2021-06-14 13:10:31 -0600

    switched to Javascript. Added functionality to selecting options and image dynamically rendering

--- src/assets/scripts/templates/collection.js
+++ src/assets/scripts/templates/collection.js
@@ -5,6 +5,119 @@ import "../../styles/templates/collection.scss";
 
 // Your Code Here
 
+const collectionList = document.querySelector(".collection-list");
+getAllProducts("test-collection").then((products) => {
+	for (let i = 0; i < products.length; i++) {
+		const productItem = document.createElement("li");
+		const productCard = document.createElement("div");
+		const productImage = document.createElement("img");
+		const productInfo = document.createElement("div");
+		const productName = document.createElement("p");
+		const productPrice = document.createElement("p");
+		const productOptions = document.createElement("div");
+
+		productItem.className = "product-item";
+		productCard.className = "product-card";
+		productImage.className = "product-image";
+		productInfo.className = "product-info";
+		productPrice.className = "product-text";
+		productName.className = "product-text";
+		productOptions.className = "product-options";
+
+		productImage.src = products[i].variants[0].image.src;
+		productName.innerText = products[i].title;
+		productPrice.innerText = `$${products[i].variants[0].price}`;
+
+		productInfo.appendChild(productName);
+		productInfo.appendChild(productPrice);
+		productInfo.appendChild(productOptions);
+		productCard.appendChild(productImage);
+		productCard.appendChild(productInfo);
+		collectionList.appendChild(productCard);
+
+		renderOptionBubbles(products[i], productOptions, productImage);
+	}
+});
+
+const getColors = (product) =>
+	product.options.filter((option) => option.name == "Color");
+
+function findVariantPicture(selectedColor, product, element) {
+	const colors = getColors(product);
+	let splitSelected = selectedColor.split(" ");
+	let selected;
+
+	if (splitSelected.length > 1) {
+		selected = splitSelected.join("-");
+	} else {
+		selected = selectedColor;
+	}
+
+	const joinedSelection = splitSelected.join("-");
+	console.log(joinedSelection);
+	for (let i = 0; i < product.variants.length; i++) {
+		const splitURL = product.variants[i].image.src.split("_");
+		for (let j = 0; j < colors[0]?.values?.length; j++) {
+			if (splitURL[1] == selected.toLowerCase()) {
+				element.src = splitURL.join("_");
+			}
+		}
+	}
+}
+
+const setBubbleColor = (color, element) => {
+	switch (color) {
+		case "Yellow":
+			element.style.backgroundColor = "#FEC109";
+			break;
+		case "Red":
+			element.style.backgroundColor = "#EF5350";
+			break;
+		case "Blue":
+			element.style.backgroundColor = "#00BCD3";
+			break;
+		case "Gold":
+			element.style.backgroundColor = "#FEC109";
+			break;
+		case "Brown":
+			element.style.backgroundColor = "#AF806E";
+			break;
+		case "Medium Grey":
+			element.style.backgroundColor = "#CDCDCD";
+			break;
+		case "medium-grey":
+			element.style.backgroundColor = "#CDCDCD";
+			break;
+		case "Navy":
+			element.style.backgroundColor = "#2F3676";
+			break;
+		case "Light Wash":
+			element.style.backgroundColor = "#00BCD3";
+			break;
+		case "Dark Wash":
+			element.style.backgroundColor = "#2F3676";
+			break;
+		default:
+			element.style.backgroundColor = "none";
+	}
+};
+
+function renderOptionBubbles(product, container, element) {
+	const colors = getColors(product);
+	colors[0]?.values?.forEach((color) => {
+		const optionInput = document.createElement("button");
+		optionInput.value = `${color}`;
+		optionInput.className = "option-bubble";
+		optionInput.style.background = "none";
+		optionInput.addEventListener("click", function (e) {
+			console.log(e.target.value);
+			findVariantPicture(e.target.value, product, element);
+		});
+		setBubbleColor(color, optionInput);
+		container.appendChild(optionInput);
+	});
+}
+
 Shopify.queryParams = {};
 if (location.search.length) {
 	for (
